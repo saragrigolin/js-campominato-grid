a741bca345b9315d20903a523cac8ff1731f42ed
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-01 13:44:52 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+Esercizio di oggi: Griglia Campo Minato
+nome repo: js-campominato-grid
+Consegna
+L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+Consigli del giorno: :party_wizard:
+Facciamo prima la parte grafica a mano e solo dopo passiamo allo script
+Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Ad esempio:
+Di cosa ho bisogno per generare i numeri?
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli in un secondo momento.
+*/
