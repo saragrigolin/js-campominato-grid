00c41e2c3d50432ca9de6bed623477fce4b3301c
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-01 16:06:08 +0100

    add random number to square

--- js/script.js
+++ js/script.js
@@ -18,6 +18,13 @@ Le validazioni e i controlli possiamo farli in un secondo momento.
 
 //l'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range
 
+//funzione per randomizzare numeri dentro ai quadrati
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
 //prendo il pulsante Play dal DOM
 const buttonPlay = document.getElementById('play');
 let containerGrid = document.getElementById('grid');
@@ -27,13 +34,16 @@ buttonPlay.addEventListener('click', function () {
     grid.innerHTML = '';
     //controllo valori inseriti dall'utente nella select
     const level = document.getElementById('level').value;
-    //calcolo larghezza griglia
+
+    //dichiaro le colonne e le righe
     let row = 0;
     let col = 0;
 
+    //in base al livello di difficoltà, cambiano le colonne e le righe
     if (level == 'easy'){
         row = 7;
         col = 7;
+
     } else if (level == 'intermediate') {
         row = 9;
         col = 9;
@@ -41,7 +51,14 @@ buttonPlay.addEventListener('click', function () {
         row = 10;
         col = 10;
     }
+
+    //calcolo grandezza della griglia e i numeri random da inserire dentro
     let numberSquare = row * col;
+    let maxNumberSquare = numberSquare;
+    let minNumberSquare = 1;
+    const numbersRandom = [];
+    
+    //genero i quadrati in base al numero di righe e colonne
     for (let index = 0; index < numberSquare; index++) {
         const square = document.createElement('div');
         square.classList.add('square');
@@ -49,6 +66,17 @@ buttonPlay.addEventListener('click', function () {
         square.style.height = `calc(100% / ${row})`;
         containerGrid.append(square);
 
+        //finché il numero randomico c'è già nell'array, ne creo un altro. Altrimenti lo pusho nell'array
+        let numberRandom = getRandomIntInclusive(minNumberSquare, maxNumberSquare);
+        while (numbersRandom.includes(numberRandom)){
+            numberRandom = getRandomIntInclusive(minNumberSquare, maxNumberSquare);
+        }
+        numbersRandom.push(numberRandom);
+
+        //inserisco il numero random nel quadrato
+        square.append(numberRandom);
+
+        //al click del quadrato, diventa azzurro
         square.addEventListener('click', function () {
             this.classList.add('active');
         })
