acdc48e4d596345f6370d4b269c7057526a7bf2c
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-01 16:19:10 +0100

    rmv random numbers (incomprensione sulla consegna)

--- js/script.js
+++ js/script.js
@@ -18,13 +18,6 @@ Le validazioni e i controlli possiamo farli in un secondo momento.
 
 //l'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range
 
-//funzione per randomizzare numeri dentro ai quadrati
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
-
 //prendo il pulsante Play dal DOM
 const buttonPlay = document.getElementById('play');
 let containerGrid = document.getElementById('grid');
@@ -54,10 +47,7 @@ buttonPlay.addEventListener('click', function () {
 
     //calcolo grandezza della griglia e i numeri random da inserire dentro
     let numberSquare = row * col;
-    let maxNumberSquare = numberSquare;
-    let minNumberSquare = 1;
-    const numbersRandom = [];
-    
+
     //genero i quadrati in base al numero di righe e colonne
     for (let index = 0; index < numberSquare; index++) {
         const square = document.createElement('div');
@@ -66,15 +56,8 @@ buttonPlay.addEventListener('click', function () {
         square.style.height = `calc(100% / ${row})`;
         containerGrid.append(square);
 
-        //finché il numero randomico c'è già nell'array, ne creo un altro. Altrimenti lo pusho nell'array
-        let numberRandom = getRandomIntInclusive(minNumberSquare, maxNumberSquare);
-        while (numbersRandom.includes(numberRandom)){
-            numberRandom = getRandomIntInclusive(minNumberSquare, maxNumberSquare);
-        }
-        numbersRandom.push(numberRandom);
-
-        //inserisco il numero random nel quadrato
-        square.append(numberRandom);
+        //inserisco il numero crescente nei quadrati
+        square.append(index+1);
 
         //al click del quadrato, diventa azzurro
         square.addEventListener('click', function () {
