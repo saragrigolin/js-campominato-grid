7017f3eea4ee8856a8b7ea82f8b9aafa3de77aa3
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-12-02 09:13:44 +0100

    add script for bombs and reload function

--- js/script.js
+++ js/script.js
@@ -28,20 +28,26 @@ buttonPlay.addEventListener('click', function () {
     //controllo valori inseriti dall'utente nella select
     const level = document.getElementById('level').value;
 
-    //dichiaro le colonne e le righe
+    //dichiaro le colonne, le righe, l'array di numeri dei quadrati e l'array delle bombe
     let row = 0;
     let col = 0;
+    let numbers = [];
+    let blackListNumbers = [5, 13, 27, 41, 49, 68, 80];
+    
 
-    //in base al livello di difficoltà, cambiano le colonne e le righe
+    //in base al livello di difficoltà, cambiano le colonne e le righe e i numeri che equivalgono alle bombe
     if (level == 'easy'){
         row = 10;
         col = 10;
+        
     } else if (level == 'intermediate') {
         row = 9;
         col = 9;
+        
     } else if (level == 'difficult') {
         row = 7;
         col = 7;
+        
     }
 
     //calcolo grandezza della griglia e i numeri random da inserire dentro
@@ -49,18 +55,55 @@ buttonPlay.addEventListener('click', function () {
 
     //genero i quadrati in base al numero di righe e colonne
     for (let index = 0; index < numberSquare; index++) {
+
         const square = document.createElement('div');
         square.classList.add('square');
         square.style.width = `calc(100% / ${col})`;
         square.style.height = `calc(100% / ${row})`;
+
+        //inserisco i quadrati nella griglia
         containerGrid.append(square);
+        numbers.push(index+1);
 
         //inserisco il numero crescente nei quadrati
-        square.append(index+1);
+        square.append(numbers[index]);
 
+        //se dentro ai quadrati c'è un numero che corrisponde all'array della blacklist, aggiungo la classe blacklist
+        if (blackListNumbers.includes(parseInt(square.innerText))) {
+            square.classList.add('black-list');
+        }
+        
         //al click del quadrato, diventa azzurro
         square.addEventListener('click', function () {
-            this.classList.add('active');
+
+            // se il bottone è black-list
+            if (square.classList.contains('black-list')){
+
+                //aggiunge classe active-red a tutti i black-list
+                let element = document.querySelectorAll('.black-list');
+                for (let index = 0; index < element.length; index++) {
+                    element[index].classList.add('active-red');
+                }
+                //esce la scritta per aver perso
+                let h4 = `
+                    <div class="lost"> 
+                        <h4>Oh no, hai perso! Vuoi fare un'altra partita?</h4>
+                        <button class="btn-blue" type="submit" onClick="refreshPage()">Gioca di nuovo</button>
+                    </div>`;
+
+                //mostra il messaggio con un timeout di 800ms
+                setTimeout(function () {
+                    grid.innerHTML += h4;
+                }, 800);
+            } else { //altrimenti aggiunge active
+                this.classList.add('active');
+            }
         })
     }
 })
+
+//funzione per aggiornare la pagina e giocare di nuovo
+function refreshPage() {
+    window.location.reload();
+}
+
